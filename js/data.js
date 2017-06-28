babe096bf49a54f35da89fc85eacc62c47421a0f
Matt Palumbo <DukGwang@Matthews-iMac.local>
2017-06-28 08:58:13 -0400

    added data file

---
+++ js/data.js
@@ -0,0 +1,25 @@
+
+let Data = {
+
+	lists: [
+		{
+			title: 'List 1',
+			cards: ['Card 1', 'Card 2', 'Card 3'],
+			id: 0
+		},
+		{
+			title:  'List 2',
+			cards: ['Card 1', 'Card 2', 'Card 3'],
+			id: 1
+
+		},
+		{
+			title: 'List 3',
+			cards: ['Card 1', 'Card 2', 'Card 3'],
+			id: 2
+		}
+	]	
+	
+}
+
+module.exports = Data;	
